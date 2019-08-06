7c58bc53bd730caed120c697123602a046233a75
Eduardo Ojeda <eduojeda@gmail.com>
2019-08-06 17:34:12 +0200

    Improved contact form handling

--- contact-form.js
+++ contact-form.js
@@ -1,28 +1,84 @@
-window.onload = function() {
-  var form = document.getElementById("contact-us").getElementsByTagName("form")[0];
+window.onload = () => {
+  const form = document.querySelector("#contact-us form");
   form.onsubmit = formSubmit;
 };
 
 function formSubmit(e) {
   e.preventDefault();
+  resetNotifications();
 
-  var form = e.target;
-  var name = form.elements["contact-us-name"].value;
-  var email = form.elements["contact-us-email"].value;
-  var message = form.elements["contact-us-message"].value;
+  const form = e.target;
+  const name = form.elements["contact-us-name"];
+  const email = form.elements["contact-us-email"];
+  const message = form.elements["contact-us-message"];
 
-  sendMessage(name, email, message);
+  if (fieldsValid([name, email, message])) {
+    sendContactMessage(name.value, email.value, message.value);
+  }
 }
 
-function sendMessage(name, email, message) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://codebonsai.io?name=" + name + "&email=" + email + "&message=" + message);
-  xhr.addEventListener("load", function() {
-    console.log(xhr.responseText);
-  });
-  xhr.addEventListener("error", function() {
-    console.log("An error occurred while submitting the contact message.");
-  });
-  
-  xhr.send();
+function fieldsValid(fields) {
+  for (let f in fields) {
+    if (fields[f].value == "") {
+      showValidationError(fields[f]);
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function showValidationError(field) {
+  field.parentElement.querySelector("span").classList.remove("hidden");
+}
+
+function sendContactMessage(name, email, message) {
+  const form = document.querySelector("#contact-us form");
+  const data = {name, email, message};
+
+  disableSubmit();
+
+  fetch(form.action, {
+    method: form.method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(status)
+  .then(showSubmitSuccess)
+  .catch(showSubmitError)
+  .finally(enableSubmit)
+}
+
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+function disableSubmit() {
+  const submit = document.querySelector("#contact-us form button");
+  submit.disabled = true;
+  submit.classList.add("disabled");
+}
+
+function enableSubmit() {
+  const submit = document.querySelector("#contact-us form button");
+  submit.disabled = false;
+  submit.classList.remove("disabled");
+}
+
+function showSubmitSuccess() {
+  document.querySelector("#contact-us .submit .success").classList.remove("hidden");
+}
+
+function showSubmitError() {
+  document.querySelector("#contact-us .submit .error").classList.remove("hidden");
+}
+
+function resetNotifications() {
+  document.querySelectorAll("#contact-us .error, #contact-us .success").forEach(element => element.classList.add("hidden"));
 }
